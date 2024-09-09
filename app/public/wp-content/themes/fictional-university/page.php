674be75d5f5e93efb0750f8f61680bfe5ee80300
Samuel Torres <rilla@MacBook-Pro.local>
2024-09-09 19:59:19 -0400

    wrapped up section 5.16 through 5.19

--- app/public/wp-content/themes/fictional-university/page.php
+++ app/public/wp-content/themes/fictional-university/page.php
@@ -2,6 +2,8 @@
 
   get_header();
 
+  $theParent = wp_get_post_parent_id(get_the_ID());
+
   while(have_posts()) {
     the_post(); ?>
     
@@ -17,7 +19,7 @@
 
     <div class="container container--narrow page-section">
     <?php 
-      if(wp_get_post_parent_id(get_the_ID())) {
+      if($theParent) {
         ?>
         <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
@@ -30,13 +32,38 @@
       <?php
       }  
     ?>
-      <!-- <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    <?php 
+      $isParent = get_pages(array(
+        "child_of" => get_the_ID()
+      ));
+    
+      if($theParent or $isParent){ ?>
+
+
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent)?>"><?php echo get_the_title($theParent)?></a></h2>
         <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
+          <!-- <li class="current_page_item"><a href="#">Our History</a></li>
+          <li><a href="#">Our Goals</a></li> -->
+          <?php 
+
+            // associative array = 
+            // $animalSounds = array (
+            //   "cat" => "meow",
+            //   "dog" => "woof",
+            //   "cow" => "moo"
+            // );
+            
+
+            wp_list_pages(array(
+              "title_li" => null,
+              "child_of" => ($theParent) ?  $theParent : get_the_ID(),
+              "sort_column" => "menu_order" // order by wordpress admin ordering 
+            ));
+          ?>
         </ul>
-      </div> -->
+      </div>
+      <?php } ?>
 
       <div class="generic-content">
         <?php the_content(); ?>
